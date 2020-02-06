5613f6d390d148b97e8ab968a4c69c5919464dbd
Anna <filatovaa@yahoo.com>
2020-02-05 19:49:34 -0800

    AJAX request working

--- dev-books/src/components/Books.js
+++ dev-books/src/components/Books.js
@@ -1,34 +1,22 @@
 import React, { Component } from 'react';
 
 class Books extends Component {
-    state ={
+
+    state = {
         books:[]
     }
-
-    componentDidMount(){
-        this.getBooks()
-    }
-
-    getBooks = () =>{
-        fetch('http://localhost:3000/books')
-        .then(response =>response.json())
-        //this line of code is passing in the info from our ruby file via json AJAX requeest 
-        .then(json => this.setState({ books: json }))
-    .catch(error => console.log(error))
-}
-
+    
     render () {
-        console.log(this.state.books)
         return (
             <div>
-                {this.state.books.map( book => {
+                {this.props.content.map( book => {
                     return (
                     <div key={book.id} className='book'>
                         <h3>{book.name}</h3>
                         <h4>{book.author}</h4>
                         <h5>{book.published}</h5>
                         <h5>Amazon Rating : {book.customerReviews}</h5>
-                        <img src= {book.img}></img>
+                        <img src= {book.img} alt=''></img>
 
                     </div>
                     )
